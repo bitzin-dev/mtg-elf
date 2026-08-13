df263df3dffa45cbc937869ff87ad34cf729f72d
bitzin-dev <bitzindev@proton.me>
2026-08-13 17:08:22 -0300

    fix: add domain colecaomtg.exe.xyz in proxy

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: [''],
+        allowedHosts: ['colecaomtg.exe.xyz'],
         proxy: {
           '/api': {
             target: env.VITE_API_PROXY_TARGET || 'http://localhost:4000',
